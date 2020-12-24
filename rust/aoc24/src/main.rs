eb36b5e0f513bf6f13a9e1f11e17593e2116420f
Joshua L. Adelman <joshua.adelman@gmail.com>
2020-12-24 12:24:59 -0500

    simplify and speed-up rust aoc24

--- rust/aoc24/src/main.rs
+++ rust/aoc24/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::HashSet;
 use std::io::{self, Read};
 
 use peg;
@@ -8,7 +8,7 @@ type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 // Excellent resource for hexagonal grids
 // https://www.redblobgames.com/grids/hexagons
 
-const DIR: [(isize, isize, isize); 6] = [
+const OFFSETS: [(isize, isize, isize); 6] = [
     (1, -1, 0),
     (-1, 1, 0),
     (0, 1, -1),
@@ -59,7 +59,7 @@ fn main() -> Result<()> {
 }
 
 fn part1(input: &str) -> Result<()> {
-    let mut tiles = HashMap::new();
+    let mut tiles = HashSet::new();
 
     for line in input.lines() {
         let moves = hextile_offset_parser::parse(line)?;
@@ -71,32 +71,29 @@ fn part1(input: &str) -> Result<()> {
             acc
         });
 
-        let e = tiles.entry(t).or_insert(false);
-        (*e) = !(*e);
+        if !tiles.remove(&t) {
+            tiles.insert(t);
+        }
     }
 
-    let black_count = tiles.values().map(|&x| if x { 1 } else { 0 }).sum::<u64>();
+    let black_count = tiles.len();
 
     println!("part 1 solution: {}", black_count);
     Ok(())
 }
 
-type Tiles = HashMap<(isize, isize, isize), bool>;
+type Tiles = HashSet<(isize, isize, isize)>;
 
 fn flip(tile: &(isize, isize, isize), tiles: &Tiles) -> bool {
-    let cnt = DIR.iter().fold(0, |mut acc, off| {
-        match tiles
-            .get(&(tile.0 + off.0, tile.1 + off.1, tile.2 + off.2))
-            .unwrap_or(&false)
-        {
-            true => acc += 1,
-            false => (),
+    let cnt = OFFSETS.iter().fold(0, |mut acc, off| {
+        if tiles.contains(&(tile.0 + off.0, tile.1 + off.1, tile.2 + off.2)) {
+            acc += 1
         }
 
         acc
     });
 
-    let status = tiles.get(tile).or(Some(&false)).unwrap();
+    let status = tiles.contains(tile);
 
     match status {
         true => match cnt {
@@ -112,60 +109,35 @@ fn flip(tile: &(isize, isize, isize), tiles: &Tiles) -> bool {
 }
 
 fn update_day(tiles: &mut Tiles) {
-    // find min max directions
-    let mut x_max = std::isize::MIN;
-    let mut x_min = std::isize::MAX;
-    let mut y_max = std::isize::MIN;
-    let mut y_min = std::isize::MAX;
-    let mut z_max = std::isize::MIN;
-    let mut z_min = std::isize::MAX;
+    let mut tile_candidates = HashSet::new();
 
-    for t in tiles.keys() {
-        if t.0 > x_max {
-            x_max = t.0
-        } else if t.0 < x_min {
-            x_min = t.0
-        }
+    tile_candidates.extend(tiles.iter());
 
-        if t.1 > y_max {
-            y_max = t.1
-        } else if t.1 < y_min {
-            y_min = t.1
-        }
-
-        if t.2 > z_max {
-            z_max = t.2
-        } else if t.2 < z_min {
-            z_min = t.2
+    for tile in tiles.iter() {
+        for d in OFFSETS.iter() {
+            tile_candidates.insert((tile.0 + d.0, tile.1 + d.1, tile.2 + d.2));
         }
     }
 
     let mut changes = Vec::new();
-    for x in x_min - 1..x_max + 2 {
-        for y in y_min - 1..y_max + 2 {
-            for z in z_min - 1..z_max + 2 {
-                let tc = (x, y, z);
-                let do_flip = flip(&tc, tiles);
+    for tc in tile_candidates.iter() {
+        let do_flip = flip(&tc, tiles);
 
-                if do_flip {
-                    changes.push((tc, do_flip));
-                }
-            }
+        if do_flip {
+            changes.push(*tc);
         }
     }
 
-    for (tile, do_flip) in changes.iter() {
-        let e = tiles.entry(*tile).or_insert(false);
-        if *do_flip {
-            (*e) = !(*e);
+    for tile in changes.iter() {
+        if !tiles.remove(tile) {
+            tiles.insert(*tile);
         }
     }
 }
 
 fn part2(input: &str) -> Result<()> {
-    let mut tiles = HashMap::new();
+    let mut tiles = HashSet::new();
 
-    // Get initial layout
     for line in input.lines() {
         let moves = hextile_offset_parser::parse(line)?;
 
@@ -176,8 +148,9 @@ fn part2(input: &str) -> Result<()> {
             acc
         });
 
-        let e = tiles.entry(t).or_insert(false);
-        (*e) = !(*e);
+        if !tiles.remove(&t) {
+            tiles.insert(t);
+        }
     }
 
     // living art
@@ -185,7 +158,7 @@ fn part2(input: &str) -> Result<()> {
         update_day(&mut tiles);
     }
 
-    let black_count = tiles.values().map(|&x| if x { 1 } else { 0 }).sum::<u64>();
+    let black_count = tiles.len();
     println!("part 2 solution: {}", black_count);
 
     Ok(())
